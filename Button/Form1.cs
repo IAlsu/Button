d5108687d9e54cceb35e53d487ae47ab50e6c424
Alsu Islamova <Alsu_Islamova@epam.com>
2016-10-27 17:48:44 +0600

    Add button task.

---
+++ Button/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //no need for code
+        }
+
+        private void button1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point(rnd.Next(this.Width - button1.Width), rnd.Next(this.Height - button1.Height));
+        }
+    }
+}
+
